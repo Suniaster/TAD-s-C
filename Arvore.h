40b10a7480ac1f580f4a21bd2370c34d341c60c6
Suniaster <thibousbr@gmail.com>
2018-11-09 18:20:16 -0200

    Organizado melhor arquivo.h

--- Arvore.h
+++ Arvore.h
@@ -7,15 +7,17 @@ typedef struct TreeNode{
     void* inf;
 }Tnode;
 
+/* Estrutura Molde para uso da árvore AVL*/ 
 typedef struct Something{
     int key;
     // Mais coisa pra baixo
 }Geral;
-
+/* Métodos relacionados a árvore AVL */
 int InsereAVL(Tnode** N, void *aInserir);
-void FreeArvore(Tnode **N);
 void* ProcuraAVL(Tnode *N, int aProcurar);
+void FreeArvore(Tnode **N);
 
+/* Formas diferentes de Print'ss */
 void printInOrder(Tnode *N);
 void printPosOrder(Tnode *N);
 void printPreOrder(Tnode *N);
