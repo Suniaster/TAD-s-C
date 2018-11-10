ebfaeff10d1b1f539389c0dc954a0804aa452447
Suniaster <thibousbr@gmail.com>
2018-11-10 21:21:36 -0200

    Adicionado atributo de altura para os nós da arvore, e feitos novos comentarios

--- main.c
+++ main.c
@@ -2,31 +2,33 @@
 #include <stdlib.h>
 #include "Arvore.h"
 #include <time.h>
+
+#define SIZE 1048576
 int main(){
 
     Geral** Varios;
     Tnode* Root =NULL;
     srand(time(NULL));
-    Varios = (Geral**)malloc(15*sizeof(Geral*));
+    Varios = (Geral**)malloc((SIZE+10)*sizeof(Geral*));
     /* Testando inserção de elementos ordenados */
-    for(int i=0;i<10;i++){
+    for(int i=0;i < SIZE ;i++){
         Varios[i] = (Geral*)malloc(sizeof(Geral));
         Varios[i]->key = i;
         InsereAVL(&Root, Varios[i]);
     }
 
     /* Testando inserção de elementos desordeandos */
-    Varios[10] = (Geral*)malloc(sizeof(Geral));
-    Varios[11] = (Geral*)malloc(sizeof(Geral));
-    Varios[12] = (Geral*)malloc(sizeof(Geral));
+    Varios[SIZE] = (Geral*)malloc(sizeof(Geral));
+    Varios[SIZE+1] = (Geral*)malloc(sizeof(Geral));
+    Varios[SIZE+2] = (Geral*)malloc(sizeof(Geral));
     
-    Varios[10]->key = 58;
-    Varios[11]->key = 5;
-    Varios[12]->key = 7;
+    Varios[SIZE]->key = 58;
+    Varios[SIZE+1]->key = 5;
+    Varios[SIZE+2]->key = 7;
 
-    InsereAVL(&Root, Varios[10]);
-    InsereAVL(&Root, Varios[11]);
-    InsereAVL(&Root, Varios[12]);  
+    InsereAVL(&Root, Varios[SIZE]);
+    InsereAVL(&Root, Varios[SIZE+1]);
+    InsereAVL(&Root, Varios[SIZE+2]);  
 
     /* Conferindo Formato da arvore */
     printPreOrder(Root);
